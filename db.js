dbffb97ba4de35a372352c71e3e024292722dfc4
Simon Chen <presenter.simon@gmail.com>
2022-07-03 16:56:57 -0700

    first Heroku push

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var mongoDBURL = 'mongodb+srv://simon:0BOWIiQ3NTWLz1oz@cluster0.bbyg6.mongodb.net/CaseRooms';  //这里是database, 不是collections
+
+mongoose.connect(mongoDBURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var dbconnect = mongoose.connection
+
+dbconnect.on('error' , ()=>{
+    console.log(`Mongo DB Connection Failed`);
+})
+
+dbconnect.on('connected' , ()=>{
+    console.log(`Mongo DB Connection Successfull`);
+})
+
+module.exports = mongoose
