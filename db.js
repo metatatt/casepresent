76e6a312805e89cffff13db755956a564de4c062
Simon Chen <presenter.simon@gmail.com>
2022-07-03 18:44:38 -0700

    fix Server js, remove PORT

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-var mongoDBURL = 'mongodb+srv://simon:0BOWIiQ3NTWLz1oz@cluster0.bbyg6.mongodb.net/CaseRooms';  //这里是database, 不是collections
+var mongoDBURL = 'mongodb+srv://simon:0BOWIiQ3NTWLz1oz@cluster0.bbyg6.mongodb.net/CaseRooms';  //这里是database, 不是collections fix MongoDB
 
 mongoose.connect(mongoDBURL , {useUnifiedTopology:true , useNewUrlParser:true})
 
